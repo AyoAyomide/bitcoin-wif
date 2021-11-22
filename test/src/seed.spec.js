48609e6ff9b59695870506bcd466da8e80121e91
ayoayomide <ayoayomide124@gmail.com>
2021-11-22 15:11:38 +0000

    all test done

--- test/src/seed.spec.js
+++ test/src/seed.spec.js
@@ -16,10 +16,4 @@ describe('Mnemonic to seed', () => {
             );
         })
     })
-    context('When mnemonic is valid', () => {
-        it('should return valid seed : reference from Mastering bitcoin book', () => {
-            let result = seed('army van defense carry jealous true garbage claim echo media make crunch');
-            chai.expect(result).to.equal('5b56c417303faa3fcba7e57400e120a0ca83ec5a4fc9ffba757fbe63fbd77a89a1a3be4c67196f57c39a88b76373733891bfaba16ed27a813ceed498804c0570');
-        })
-    })
 });
