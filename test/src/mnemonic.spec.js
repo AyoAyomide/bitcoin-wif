9bdc70a452be3e3ab029469f75fceef3cdcad34c
ayoayomide <ayoayomide124@gmail.com>
2021-11-23 11:44:18 +0000

    test removed

--- test/src/mnemonic.spec.js
+++
@@ -1,8 +0,0 @@
-const chai = require('chai');
-const mnemonic = require('../../src/mnemonic');
-
-describe('Mnemonic generator', () => {
-    it('should return 12 words', () => {
-        chai.expect(mnemonic.trim().split(/\s+/g).length).to.equal(12);
-    })
-})
